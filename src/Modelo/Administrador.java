ea1dbb65613df659eae535db82be32ac4714883b
Dayanna <Dayanna@DESKTOP-UTPK3RQ>
2025-06-08 18:15:55 -0500

    Agregue la clase abstracta Usuario y sus herencias además de la clase penalidad

---
+++ src/Modelo/Administrador.java
@@ -0,0 +1,45 @@
+package Modelo;
+
+import Interfaces.IServiciosRecursos;
+
+/**
+ *
+ * @author Dayanna
+ */
+public class Administrador extends Usuario implements IServiciosRecursos {
+    public Administrador(String id_codigo, String contraseña, String nombre, String apellido) {
+        this.id_codigo = id_codigo;
+        this.contraseña = contraseña;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.tipoDeUser = "Administrador";
+    }
+
+    public void agregarMaterial() {
+        System.out.println("Material agregado.");
+    }
+
+    public void agregarRecursoTecnologico() {
+        System.out.println("Recurso tecnológico agregado.");
+    }
+
+    public void gestionarRecursos() {
+        System.out.println("Recursos gestionados.");
+    }
+
+    public void verificarReservas() {
+        System.out.println("Reservas verificadas.");
+    }
+
+    public void generadorReporte() {
+        System.out.println("Reporte generado.");
+    }
+
+    public void exportarInfo() {
+        System.out.println("Información exportada.");
+    }
+
+    public void mostrarInfo() {
+        System.out.println("Administrador: " + nombre + " " + apellido);
+    }
+}
---
+++ src/Modelo/Penalidad.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Dayanna
+ */
+public class Penalidad {
+    private Date fechaAplicacion;
+    private String motivo;
+    private int diasRetraso;
+    private Usuario usuario;
+
+    public Penalidad(Date fechaAplicacion, String motivo, int diasRetraso, Usuario usuario) {
+        this.fechaAplicacion = fechaAplicacion;
+        this.motivo = motivo;
+        this.diasRetraso = diasRetraso;
+        this.usuario = usuario;
+    }
+
+    public Date getFechaAplicacion() { return fechaAplicacion; }
+    public void setFechaAplicacion(Date fechaAplicacion) { this.fechaAplicacion = fechaAplicacion; }
+
+    public String getMotivo() { return motivo; }
+    public void setMotivo(String motivo) { this.motivo = motivo; }
+
+    public int getDiasRetraso() { return diasRetraso; }
+    public void setDiasRetraso(int diasRetraso) { this.diasRetraso = diasRetraso; }
+
+    public Usuario getUsuario() { return usuario; }
+    public void setUsuario(Usuario usuario) { this.usuario = usuario; }
+
+    public void aplicar() {
+        usuario.getPenalidades().add(this);
+        System.out.println("Penalidad aplicada a " + usuario.getNombre());
+    }
+}
