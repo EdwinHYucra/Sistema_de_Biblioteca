463d6117869267ac492b72f25e2f039c857fd759
user <user@192.168.1.25>
2025-06-08 20:11:33 -0500

    Interface bloqueo con la clase Reserva de ambiente y sala

--- src/Modelo/ReservaDeAmbiente.java
+++ src/Modelo/ReservaDeAmbiente.java
@@ -4,7 +4,10 @@ package Modelo;
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+
 import Interfaces.IBloqueo;
+import java.util.ArrayList;
+import java.util.List;
 /**
  *
  * @author user
@@ -14,16 +17,19 @@ public class ReservaDeAmbiente implements IBloqueo {
     private int capacidadMax;
     private String estado = "Disponible";
     private int tiempoRestante;
+    private List<Alumno> listaAlumnos;
+    private Sala sala;
 
-    public ReservaDeAmbiente(String codigo, int capacidad, int tiempoEnMinutos) {
-        if (tiempoEnMinutos < 0 || tiempoEnMinutos > 120) {
-            throw new IllegalArgumentException("Tiempo debe estar entre 60 y 120 minutos.");
+    public ReservaDeAmbiente(String codigo, int capacidad, int tiempoEnMinutos, Sala sala) {
+        if (tiempoEnMinutos < 60 || tiempoEnMinutos > 120) {
+            throw new IllegalArgumentException("El tiempo debe estar entre 60 y 120 minutos.");
         }
 
         this.codigo = codigo;
         this.capacidadMax = capacidad;
         this.tiempoRestante = tiempoEnMinutos;
-
+        this.sala = sala;
+        this.listaAlumnos = new ArrayList<>();
     }
 
     public String getCodigo() {
@@ -57,17 +63,40 @@ public class ReservaDeAmbiente implements IBloqueo {
     public void setTiempoRestante(int tiempoRestante) {
         this.tiempoRestante = tiempoRestante;
     }
-    
+
+    public List<Alumno> getListaAlumnos() {
+        return listaAlumnos;
+    }
+
+    public void setListaAlumnos(List<Alumno> listaAlumnos) {
+        this.listaAlumnos = listaAlumnos;
+    }
+
+    public Sala getSala() {
+        return sala;
+    }
+
+    public void setSala(Sala sala) {
+        this.sala = sala;
+    }
+
+    public void agregarAlumno(Alumno alumno) {
+        if (listaAlumnos.size() >= capacidadMax) {
+            System.out.println("No se puede agregar más alumnos. Capacidad máxima alcanzada.");
+        } else {
+            listaAlumnos.add(alumno);
+            System.out.println("Alumno agregado: " + alumno.getNombre());
+        }
+    }
+
     public void verificarEstado() {
         System.out.println("Sala [" + codigo + "] - Estado: " + estado + " - Tiempo restante: " + tiempoRestante + " min");
     }
-
 
     @Override
     public void restringirAcceso() {
         estado = "Bloqueada";
         tiempoRestante = 0;
-        System.out.println(" Sala [" + codigo + "] bloqueada por tiempo expirado.");
+        System.out.println("Sala [" + codigo + "] bloqueada por tiempo expirado.");
     }
 }
-     
