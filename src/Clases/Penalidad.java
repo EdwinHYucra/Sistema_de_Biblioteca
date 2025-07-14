e9388d8a9b1a2b250ff335e94fb1a3b1ec63b061
user <user@192.168.1.25>
2025-07-13 22:34:10 -0500

    Modificacion clase penalidad

--- src/Clases/Penalidad.java
+++ src/Clases/Penalidad.java
@@ -4,6 +4,7 @@
  */
 package Clases;
 
+import java.time.LocalDate;
 import java.util.Date;
 
 /**
@@ -11,20 +12,26 @@ import java.util.Date;
  * @author Dayanna
  */
 public class Penalidad {
-    private Date fechaAplicacion;
+    private LocalDate fechaAsignacion;
+    private LocalDate fechaFinalizacion;
     private String motivo;
     private int diasRetraso;
     private Usuario usuario;
 
-    public Penalidad(Date fechaAplicacion, String motivo, int diasRetraso, Usuario usuario) {
-        this.fechaAplicacion = fechaAplicacion;
+    public Penalidad(LocalDate fechaAsignacion, LocalDate fechaFinalizacion, String motivo, int diasRetraso, Usuario usuario ) {
+        this.fechaAsignacion = fechaAsignacion;
+        this.fechaFinalizacion = fechaFinalizacion;
         this.motivo = motivo;
         this.diasRetraso = diasRetraso;
         this.usuario = usuario;
     }
 
-    public Date getFechaAplicacion() { return fechaAplicacion; }
-    public void setFechaAplicacion(Date fechaAplicacion) { this.fechaAplicacion = fechaAplicacion; }
+    public LocalDate getFechaAsignacion() { return fechaAsignacion; }
+    public void setFechaAplicacion(LocalDate fechaAplicacion) { this.fechaAsignacion = fechaAplicacion; }
+
+    public LocalDate getFechaFinalizacion() { return fechaFinalizacion; }
+
+    public void setFechaFinalizacion(LocalDate fechaFinalizacion) { this.fechaFinalizacion = fechaFinalizacion; }
 
     public String getMotivo() { return motivo; }
     public void setMotivo(String motivo) { this.motivo = motivo; }
