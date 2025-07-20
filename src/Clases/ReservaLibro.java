8a2b0df32e7b3f9099d0e302ed8c1f2b9e10d2f0
user <user@192.168.1.25>
2025-07-20 00:35:53 -0500

    remake reserva de libro

--- src/Clases/ReservaLibro.java
+++ src/Clases/ReservaLibro.java
@@ -3,25 +3,26 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Clases;
+
 import java.time.LocalDate;
 import java.time.LocalTime;
 //import java.util.Date;
+
 /**
  *
  * @author ehuan
  */
 public class ReservaLibro extends Reserva {
-    private LocalDate fechadevolucion;
-    private Libro libro;
-    private String estado;
 
-    public ReservaLibro(LocalDate fechadevolucion, Libro libro, String estado, LocalDate fechaReserva, double duracion, LocalTime horaReserva, Usuario usuario) {
-        super(fechaReserva, duracion, horaReserva, usuario);
+    private LocalDate fechadevolucion;
+    private int libro;
+
+    public ReservaLibro(int ejemplar, int id, LocalDate fechaReserva, int tipoReserva, String usuario, String estado, LocalDate fechadevolucion) {
+        super(id, fechaReserva, tipoReserva, usuario, estado);
+        this.libro = ejemplar;
         this.fechadevolucion = fechadevolucion;
-        this.libro = libro;
-        this.estado = estado;
     }
-    
+
     public LocalDate getFechadevolucion() {
         return fechadevolucion;
     }
@@ -30,11 +31,11 @@ public class ReservaLibro extends Reserva {
         this.fechadevolucion = fechadevolucion;
     }
 
-    public Libro getLibro() {
+    public int getLibro() {
         return libro;
     }
 
-    public void setLibro(Libro libro) {
+    public void setLibro(int libro) {
         this.libro = libro;
     }
 
@@ -45,8 +46,9 @@ public class ReservaLibro extends Reserva {
     public void setEstado(String estado) {
         this.estado = estado;
     }
+
     @Override
-    public void mostrarInfo(){
+    public void mostrarInfo() {
         System.out.println("Mostrando informacion del la clase padre como la del hijo");
     }
 }
