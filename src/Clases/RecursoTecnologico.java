3dfe14d6ab473ee570fbad9b0bd7c480289fa3ce
user <user@192.168.1.25>
2025-07-20 00:34:50 -0500

    remake recurso tecnologico

--- src/Clases/RecursoTecnologico.java
+++ src/Clases/RecursoTecnologico.java
@@ -1,25 +1,40 @@
 package Clases;
 
 public class RecursoTecnologico {
-    String IDcodigo;
-//
-    public boolean VerificarDisponibilidad() {
-        return true; 
+    public int IDcodigo;
+    private String tipo;
+    private String estado;
+
+   
+    public RecursoTecnologico(int IDcodigo) {
+    this.IDcodigo = IDcodigo;
+    this.estado = "disponible";
     }
 
-    public String getIDcodigo() {
+    public boolean VerificarDisponibilidad() {
+        return "Disponible".equalsIgnoreCase(estado);
+    }
+
+    public int getIDcodigo() {
         return IDcodigo;
     }
 
-    public void setIDcodigo(String IDcodigo) {
+    public void setIDcodigo(int IDcodigo) {
         this.IDcodigo = IDcodigo;
     }
-
-    void mostrarInfo() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    
+     public void setEstado(String estado) {
+        this.estado = estado;
     }
 
-    void setEstado(String disponible) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public String getEstado() {
+        return estado;
     }
+
+   public void mostrarInfo() {
+    System.out.println("Código: " + IDcodigo + ", Tipo: " + tipo + ", Estado: " + estado);
+}
+   /*void setEstado(String disponible) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }*/
 }
